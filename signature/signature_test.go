759a3da45563ae2c2283a3744f728cc80dc38b4f
coderlang <coderlang@foxmail.com>
2023-11-09 18:04:02 +0800

    更新签名

--- signature/signature_test.go
+++ signature/signature_test.go
@@ -1,36 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"testing"
 )
 
-func Test_Sign_Plaintext(t *testing.T) {
-	var coderlangPrivateKey = "64946dbe03f1b8bcfacc95a7242da55e16cf2b9536d249414520f2cf8017ef1a"
-	key, err := crypto.GenerateKey()
+func Test_Sign_Verify(t *testing.T) {
+	var privateKeyHex = "c47a0fb020f2066223e049ea342c5ea9e9844da92b3101d37ae115d7f13380d8"
+	privateKeyBytes, err := hex.DecodeString(privateKeyHex)
 	if err != nil {
+		t.Fatal(fmt.Sprintf("decode privateKey err %v", err))
 		return
 	}
-	privateKey, err := crypto.HexToECDSA(coderlangPrivateKey)
+
+	privateKey, err := crypto.ToECDSA(privateKeyBytes)
 	if err != nil {
-		panic(fmt.Sprintf("privateKey %s err=%v", coderlangPrivateKey, err))
+		t.Fatal(fmt.Sprintf("ToECDSA privateKey err %v", err))
+		return
 	}
 	publicKey := privateKey.PublicKey
 
-	// 打印公钥
+	println("PrivateKey: ", hex.EncodeToString(privateKey.D.Bytes()))
 	println("PublicKey: ", hex.EncodeToString(crypto.CompressPubkey(&publicKey)))
 	println("Address: ", crypto.PubkeyToAddress(publicKey).Hex())
 	// 要签名的数据
-	data := []byte("小明借了100块钱给码农浪哥")
-
+	msg := "coderlang 34+ 程序员，CTO，全栈工程师，搬砖 11 年，薪资翻 20 倍，公众号 coderlang 主理人"
+	fmt.Println("Msg: ", msg)
 	// 对数据进行哈希
-	hash := crypto.Keccak256Hash(data)
-	fmt.Println("Hash: ", hex.EncodeToString(hash.Bytes()))
+	hash := crypto.Keccak256([]byte(msg))
+	fmt.Println("Hash: ", hex.EncodeToString(hash))
 	// 使用私钥对数据哈希进行签名
-	signature, err := crypto.Sign(hash.Bytes(), privateKey)
+	signature, err := crypto.Sign(hash, privateKey)
 	if err != nil {
 		fmt.Println("Error signing data:", err)
 		return
@@ -38,44 +43,62 @@ func Test_Sign_Plaintext(t *testing.T) {
 
 	// 打印签名
 	fmt.Println("Signature: ", hex.EncodeToString(signature))
-}
-
-type Msg struct {
-	From   string
-	Method string
-	To     string
-	Value  int
-}
-
-func Test_Sign_CodeStructure(t *testing.T) {
-	var coderlangPrivateKey = "64946dbe03f1b8bcfacc95a7242da55e16cf2b9536d249414520f2cf8017ef1a"
-	privateKey, err := crypto.HexToECDSA(coderlangPrivateKey)
+	sigToPub, err := crypto.SigToPub(hash, signature)
 	if err != nil {
-		panic(fmt.Sprintf("privateKey %s err=%v", coderlangPrivateKey, err))
-	}
-	publicKey := privateKey.PublicKey
-
-	// 打印公钥
-	println("PublicKey: ", hex.EncodeToString(crypto.CompressPubkey(&publicKey)))
-	println("Address: ", crypto.PubkeyToAddress(publicKey).Hex())
-	// 要签名的数据
-	data, _ := json.Marshal(Msg{
-		From:   crypto.PubkeyToAddress(publicKey).Hex(),
-		To:     "5992d4246bB01672C8B96847342eBd5663A0f377",
-		Method: "Transfer",
-		Value:  100,
-	})
-	println("Msg: ", string(data))
-	// 对数据进行哈希
-	hash := crypto.Keccak256Hash(data)
-	fmt.Println("Hash: ", hex.EncodeToString(hash.Bytes()))
-	// 使用私钥对数据哈希进行签名
-	signature, err := crypto.Sign(hash.Bytes(), privateKey)
-	if err != nil {
-		fmt.Println("Error signing data:", err)
+		fmt.Println("Error SigToPub:", err)
 		return
 	}
 
-	// 打印签名
-	fmt.Println("Signature: ", hex.EncodeToString(signature))
+	println("SigToPub PublicKey: ", hex.EncodeToString(crypto.CompressPubkey(sigToPub)))
+
+	invalidMsg := "coderlang 34+ 程序员，CTO，全栈工程师，搬砖 11 年，薪资翻 200 倍，公众号 coderlang 主理人"
+	fmt.Println("InvalidMsg: ", invalidMsg)
+	invalidSigToPub, err := crypto.SigToPub(crypto.Keccak256([]byte(invalidMsg)), signature)
+	if err != nil {
+		fmt.Println("Error SigToPub:", err)
+		return
+	}
+	println("InvalidSigToPub PublicKey: ", hex.EncodeToString(crypto.CompressPubkey(invalidSigToPub)))
+}
+
+func Test_Encrypt_Decrypt(t *testing.T) {
+	var privateKeyHex = "c47a0fb020f2066223e049ea342c5ea9e9844da92b3101d37ae115d7f13380d8"
+
+	privateKeyBytes, err := hex.DecodeString(privateKeyHex)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("decode privateKey err %v", err))
+		return
+	}
+
+	privateKey, err := crypto.ToECDSA(privateKeyBytes)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("ToECDSA privateKey err %v", err))
+		return
+	}
+	privateKeyECC := ecies.ImportECDSA(privateKey)
+
+	println("PrivateKey: ", hex.EncodeToString(privateKeyECC.D.Bytes()))
+	println("PublicKey: ", hex.EncodeToString(crypto.CompressPubkey(privateKeyECC.PublicKey.ExportECDSA())))
+
+	msg := "coderlang 34+ 程序员，CTO，全栈工程师，搬砖 11 年，薪资翻 20 倍，公众号 coderlang 主理人"
+	fmt.Println("Msg: ", msg)
+
+	encrypt, err := ecies.Encrypt(rand.Reader, &privateKeyECC.PublicKey, []byte(msg), nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("PublicKey Encrypt: ", hex.EncodeToString(encrypt))
+
+	decrypt, err := privateKeyECC.Decrypt(encrypt, nil, nil)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("Decrypt err %v", err))
+		return
+	}
+
+	if !bytes.Equal(decrypt, []byte(msg)) {
+		t.Fatal("ecies: plaintext doesn't match message")
+	}
+
+	println("PrivateKey Decrypt: ", string(decrypt))
 }
